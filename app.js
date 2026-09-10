e6c031efc223caae38e26b3c50b85870b0e8b98d
Cryiing <muelsamukk@gmail.com>
2026-09-10 20:32:48 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2711,7 +2711,11 @@ const todayProductions =
       ) &&
       isToday(item.date)
   );
-
+console.log("========== RESUMO DO DIA ==========");
+console.log("HISTÓRICO TOTAL:", historyItems);
+console.log("HOJE:", new Date());
+console.log("PRODUÇÕES DE HOJE:", todayProductions);
+console.log("QUANTIDADE DE PRODUÇÕES HOJE:", todayProductions.length);
   if (
     todayProductions.length === 0
   ) {
